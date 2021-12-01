c0025e17b74e102c30f0c97c94133589496ae657
maxleclerc2 <mleclerc@et.esiea.fr>
2021-12-01 18:30:57 +0100

    Setup DB

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -2,21 +2,28 @@ package fr.lernejo.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 @RestController
 public class TodoListController {
-    private final ArrayList<Todo> todoList = new ArrayList<>();
+    private final TodoRepository repository;
+
+    public TodoListController(TodoRepository repo) {
+        this.repository = repo;
+    }
 
     @PostMapping("/api/todo")
-    public void addTodo(Todo newTodo) {
-        this.todoList.add(newTodo);
+    public void addTodo(@RequestBody TodoEntity newTodo) {
+        this.repository.save(newTodo);
     }
 
     @GetMapping("/api/todo")
-    public ArrayList<Todo> getTodoList() {
-        return todoList;
+    public ArrayList<TodoEntity> getTodoList() {
+        Iterable<TodoEntity> repo = this.repository.findAll();
+        return new ArrayList<>((Collection<? extends TodoEntity>) repo);
     }
 }
